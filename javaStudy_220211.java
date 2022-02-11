a924a17936414b812f4b0eecb236ee57f2f6b525
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-02-12 02:26:59 +0900

    Create javaStudy_220211.java
    접미사 기초

---
+++ javaStudy_220211.java
@@ -0,0 +1,18 @@
+package jiwon_studyAlone;
+
+public class javaStudy_220211 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("sysout + ctrl + spaceBar");
+System.out.println("자바의정석 기초 52p_리터럴의 타입과 접미사");
+
+	System.out.println("논리형: X"+"\t"+"정수형(Long): 123,Ob0101,ff00,100L ... = L"+"\t"+"실수형(float, double): 3.14, 3.0e8, 1.4f, ox1.0p-1... = f,d "+"\t"+"문자형:a,1,\n..."+"\t"+"문자열:'안녕', '스텝백'...");
+
+	String name = new String("JAVA");
+	String name1 = "JAVA";
+	System.out.println("String 은 class 요소이기 때문에 위처럼 표현 가능");
+	
+
+}

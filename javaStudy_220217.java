bbc915d3a3a26951b718e0913bb668b3594dbd3c
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-02-17 23:50:37 +0900

    Create javaStudy_220217.java
    문자열의 결합

---
+++ javaStudy_220217.java
@@ -0,0 +1,20 @@
+package jiwon_studyAlone;
+
+public class javaStudy_220217 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 220217. 아니 제발 열심히하자~! 기본이라도 !!!
+		
+		System.out.println("54p_2-08 문자열 결합");
+		System.out.println("숫자 뿐만 아니라 두 문자열을 합칠 때에도 덧셈기호를 사용할 수 있다.");
+		
+		String name = "Ji" + "Won";
+		String str = name + 8.0;
+		
+		System.out.println("name: "+name);
+		System.out.println("str: "+str);
+	}
+
+}

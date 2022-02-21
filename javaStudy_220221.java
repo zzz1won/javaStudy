41de0fd4682253a0226a615364cc9eb5bdfcc08d
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-02-21 23:57:23 +0900

    Create javaStudy_220221.java
    간만에 보는 Scanner scan, 현직에서는 잘 안쓴다지만...

---
+++ javaStudy_220221.java
@@ -0,0 +1,20 @@
+package jiwon_studyAlone;
+
+import java.util.Scanner;
+
+public class javaStudy_220221 {
+
+	public static void main(String[] args) {
+		// 월요일 열심히 살았어야했는데 ㅠ
+
+		System.out.println("61p_2-14 화면으로부터 입력받기");
+		//드디어 다시 나온 scanner scan
+		
+		Scanner scan = new Scanner (System.in);
+		// Scanner 클래스의 객체 생성
+		
+		String input = scan.nextLine(); //입력받은 내용을 input에 저장
+		int num = Integer.parseInt(input); //입력받은 내용을 int 타입의 값으로 변환
+	}
+
+}

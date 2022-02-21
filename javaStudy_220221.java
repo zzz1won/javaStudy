c88cab742f386d2ccdd8a001145742a32dce1c86
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-02-22 02:26:35 +0900

    Update javaStudy_220221.java
    scan 예제내용까지

--- javaStudy_220221.java
+++ javaStudy_220221.java
@@ -15,6 +15,15 @@ public class javaStudy_220221 {
 		
 		String input = scan.nextLine(); //입력받은 내용을 input에 저장
 		int num = Integer.parseInt(input); //입력받은 내용을 int 타입의 값으로 변환
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("두 자리 정수를 하나 입력해주세요!: ");
+		String input1 = scan.nextLine();
+		int num1 = Integer.parseInt(input1); //입력 받은 문자열을 숫자로 변환
+		
+		System.out.println("입력내용: "+input1);
+		System.out.println("num1: "+num1);
 	}
 
 }

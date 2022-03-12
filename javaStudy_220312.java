f723cd7d60f1aa905bd52720e2a2addc3630cb01
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-03-13 00:15:18 +0900

    Update javaStudy_220312.java
    연습 문제 다시!

--- javaStudy_220312.java
+++ javaStudy_220312.java
@@ -23,6 +23,7 @@ public class javaStudy_220312 {
 		char ch = '3'; // 더블쿼터 사용이 안된다!
 		
 		System.out.println("2) char형 변수 ch가 공백이거나 탭이 아닐 때 true인 조건식");
+		System.out.printf("ch는%c",ch);
 		if(!(ch==' '&&ch=='\t'))	{
 			System.out.println("2번답, true");
 		}else	{
@@ -30,7 +31,7 @@ public class javaStudy_220312 {
 		}
 		
 		System.out.println("3) char형 변수 ch가 숫자 '0'~'9'일 때 true인 조건식");
-
+		System.out.printf("ch는%c",ch);
 		if('0' <= ch && ch<'9')	{
 			System.out.println("3번답, true");
 		}else	{
@@ -38,8 +39,8 @@ public class javaStudy_220312 {
 		}
 		
 		System.out.println("4) char형 변수 ch가 영문자(대문자or소문자)일 때 true인 조건식");
-		System.out.println("ch를 'B'로 설정");
 		ch='B';
+		System.out.printf("ch는%c",ch);
 		if(('a'<=ch&&ch<='z')||('A'<=ch&&ch<='Z')) {
 			System.out.println("4번답, true");
 		}else	{
@@ -48,7 +49,7 @@ public class javaStudy_220312 {
 		
 		System.out.println("5) char형 변수 ch가 'x' 또는 'X'일 때 true인 조건식");
 		ch='x';
-	
+		System.out.printf("ch는%c",ch);
 		if(ch=='x'||ch=='X') {
 			System.out.println("5번답, true");
 		}else	{
@@ -66,22 +67,23 @@ public class javaStudy_220312 {
 		}
 	
 		System.out.println("7) boolean형 변수 powerOn이 false일 때 ture인 조건식");
-		boolean powerOn = true;
+		boolean powerOn = false;
 		
 		if(!powerOn) {
-			System.out.println("7번답, true");
+			System.out.println("7번답, power가 ture라서, 정답을 틀렸습니다");
 		}else	{
-			System.out.println("7번답, false.");
+			System.out.println("7번답, 값이 false, 정답입니다");
 		}
 		
 		System.out.println("8) 문자형 참조변수 str이 'yes'일 때 true인 조건식");
 		String str;
 		System.out.print("둘 중에 하나 골라 말하기 yes or no: ");
+		String st = scan.nextLine();
 		str=scan.nextLine();
-		if(str=="yes") {
-			System.out.println("8번답, ture");
+		if(str.equals("yes")) {
+			System.out.println("8번답, ture 입력한 값은: "+str);
 		}else	{
-			System.out.println("8번답, false");
+			System.out.println("8번답, false 입력한 값은: "+str);
 		}
 	}
 

71134a54305e83048fdb90e3bed19a10dd5630ac
zzz1won <92144729+zzz1won@users.noreply.github.com>
2022-03-16 23:54:59 +0900

    220316

---
+++ javaStudy_220316_5_1.java
@@ -0,0 +1,17 @@
+package jiwon_studyAlone;
+
+public class javaStudy_220316_5_1 {
+
+	public static void main(String[] args) {
+		// 130p
+		
+		System.out.println("5-01.배열이란?");
+		System.out.println("같은 타입의 여러 변수를 하나의 묶음으로 다루는 것. Point: 같은 타입 이어야 한다는 것이다.");
+		
+		//배열을 사용하지 않는다면, 학생 5명의 점수를 저장하기 위해서는 5개의 변수를 선언해야한다.
+		int score1, score2, score3, score4, score5 =0;
+		System.out.println("변수 대신 배열을 이용하면 배열을 사용하여 간단히 표현할 수 있다.");
+		int[] score = new int[5];
+	}
+
+}
